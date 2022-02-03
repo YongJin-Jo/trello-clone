2044bff5012d942f64f9964463ab079fb67d529d
YongJin-Jo <artinc5233@gmail.com>
2022-02-03 22:11:46 +0900

    Drag and Drop 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
-function App() {
+export function App() {
+  const onDragEnd = () => {
+    return;
+  };
   return (
-    <div className="App">
-   
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        <Droppable droppableId="one">
+          {magic => (
+            <ul ref={magic.innerRef} {...magic.droppableProps}>
+              <Draggable draggableId="first" index={0}>
+                {magic => (
+                  <li
+                    ref={magic.innerRef}
+                    {...magic.dragHandleProps}
+                    {...magic.draggableProps}
+                  >
+                    Holle
+                  </li>
+                )}
+              </Draggable>
+              <Draggable draggableId="scond" index={1}>
+                {magic => (
+                  <li
+                    ref={magic.innerRef}
+                    {...magic.dragHandleProps}
+                    {...magic.draggableProps}
+                  >
+                    Holle1
+                  </li>
+                )}
+              </Draggable>
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
   );
 }
-
-export default App;
